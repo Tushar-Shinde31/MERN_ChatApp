26c103f5b68bf413b60a19a80416668b514baaed
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2025-01-06 21:36:58 +0530

    set up frontend structure with Tailwind CSS, Axios, and basic pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import messageRoutes from "./routes/message.route.js";
+import cors from 'cors';
 
 dotenv.config();
 
@@ -15,6 +16,11 @@ app.use(express.json())
 
 app.use(cookieParser())
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+a
 app.use("/api/auth", authRoutes);
 
 app.use("/api/messages", messageRoutes);
